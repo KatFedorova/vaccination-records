12a532a4bf377f88046d1fa8aba17ff10e901a83
Kat Fedorova <firemylife@mail.ru>
2020-03-01 15:21:41 +0300

    empty

--- src/main/java/ru/fedorova/vaccination/repo/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.fedorova.vaccination.repo;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.fedorova.vaccination.model.entity.Patient;
-
-public interface PatientRepository extends CrudRepository<Patient, Integer> {
-}
